cf6aebaf77a0f0d3365405e138f214a82b982534
agusmaul96 <agusmaulanaa96@gmail.com>
2022-06-07 20:34:58 +0700

    landing page 1

--- src/component/LandingPage.js
+++ src/component/LandingPage.js
@@ -1,6 +1,5 @@
 import "./landing.css";
-import "./bootstrap.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 const LandingPage = () => {
   return (
     <div class="container-sm" id="halaman1">
