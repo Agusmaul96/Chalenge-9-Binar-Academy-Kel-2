c8e9ed93f865c927ed9f9402bd86ede9f2e314f3
unknown <agusmaulanaa96@gmail.com>
2022-06-10 22:37:35 +0700

    landing page

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -2,6 +2,7 @@ import "./homepage.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 const HomePage = () => {
   return (
+    <div>
     <div id="halaman1">
       <div id="navbar">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark opacity-75">
@@ -72,6 +73,65 @@ const HomePage = () => {
         </svg>
       </div>
     </div>
+        
+    <div class="container" id="halaman2">
+      <div>
+        <div class="row justify-content-center" id="conten2">
+          <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="kirigame">
+              <br />
+              <h4>what's so special ?</h4>
+              <br />
+              <h1>THE GAMES</h1>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-12 col-sm-12">
+            <br />
+            <br />
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="assets/rockpaperstrategy-1600.jpg" class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/rockpaperstrategy-1600.jpg" class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/rockpaperstrategy-1600.jpg" class="d-block w-100" alt="..." />
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div id="halaman3">
+      llll
+    </div>
+    <div id="halaman4">llll
+    </div>
+  
+    <div id="halaman5" >   lll
+    </div>
+    <div id="halaman6">llll
+      </div>
+    
+    </div>
+  
   );
 };
 
