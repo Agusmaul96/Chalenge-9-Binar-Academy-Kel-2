233739d6bdfabc9ccca1e25a8bca7bc4cfae9ced
Soraya Isfandiari <soraya.isf@gmail.com>
2022-06-13 14:07:55 +0700

    landingpage1-and-game

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 
-import React from "react"
+import React, { useState } from "react"
 import "./App.css";
 import HomePage from "./Pages/Homepage";
 import GameList from "./Pages/GameList";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
+import Home from './Pages/Home';
+import Play from './Pages/Play';
+import Game from './Pages/Game';
+
+
 function App() {
+  const [playerChoice, setplayerChoice] = useState("");
+  const [score, setScore] = useState(0);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="gamelist" element={<GameList/>}/>
-        <Route path="login" element={<Login/>}/>
-        <Route path="signup" element={<Signup/>}/>
-        <Route path="gameplay" element={<GameList/>}/>
-    </Routes></Router>
-      
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="gamelist" element={<GameList />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="gameplay" element={<GameList />} />
+        <Route path="/startgame" element={<Play setplayerChoice={setplayerChoice} />} />
+        <Route path="/game" element={<Game playerChoice={playerChoice} score={score} setScore={setScore} />} />
+      </Routes>
+    </Router>
   );
 }
 export default App;
---
+++ src/Pages/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/footer.css';
+
+function Footer() {
+
+    return (
+        <>
+            <div className="app__container">
+                <div className="footer">
+                    <div> Challenge by
+                        <h5>FSW-19 Group 2</h5>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
